ba3b1ffc9b9798c78a2154ffeac51aeb0fcd147c
user <user@Williams-iMac.local>
2020-05-05 20:16:11 +0500

    Added Task.Run() which colling method ShowFallingRain() with two sync simbols

--- Program.cs
+++ Program.cs
@@ -28,11 +28,48 @@ namespace HomeWork17
                     }
                 }).Wait();
                 
-                
+                Task.Run(() =>
+                {
+                    ShowFallingRain(list);
+                    list.Clear();
+
+                }).Wait();
             }
         }
 
-           
+           static void ShowFallingRain(List<Matrix> symbols)
+          {
+            int j = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                foreach (var k in symbols)
+                {
+                    Console.CursorTop = i + k.Top;
+                    foreach (var z in k.simbol)
+                    {
+                        Console.CursorLeft = k.Left;
+                        if (j == k.simbol.Length - 1)
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        else if (j == k.simbol.Length - 2)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        }
+                        Console.WriteLine($"{z}{z}");
+                       
+                        j++;
+                    }
+                    j = 0;
+                }
+                Thread.Sleep(50);
+                Console.Clear();
+            }
+        }
 
         static string Word()
         {
